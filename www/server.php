089a039aaac4ac7c59322a21c9cb81a50b8e0301
Robert-Dan <danrobert445@gmail.com>
2025-01-22 18:39:55 -0500

    default value

--- www/server.php
+++ www/server.php
@@ -19,62 +19,89 @@ define("VENDOR", ROOT . DS . "vendor" . DS);
 include(CONFIG . DS . 'bootstrap.php');
 #if (!Dispatcher::threads()) throw_exception('dispatcher_threads()');
 
-        global $aUri;
-        $aUri = parse_url('/'. $_SERVER["REQUEST_URI"]);
-        
-        switch ($aUri['host']) {
-            case 'favicon.ico':
-                header('Content-Type: text/x-icon');
-                print file_get_contents(ROOT .'www'. DS .'favicon.ico');
-                exit();
-            break;
-            case 'style':
-                header('Content-Type: text/css; charset=utf-8');
-                print file_get_contents(DRAFT .'static'. DS .$aUri['path']);
-                exit();
-            break;
-            case 'script':
-                header('Content-Type: text/javascript; charset=utf-8');
-                print file_get_contents(DRAFT .'static'. DS .$aUri['path']);
-                exit();
-            break;
-            case 'index':
-                return true;
-            break;
-        }
-        
-        $aWidget['html'] = '';
-        #$aWidget['html'] .= '<!--- doctype -->';
-        #$aWidget['html'] .= '<!doctype html>';
-        $aWidget['html'] .= '<!--- html -->';
-        $aWidget['html'] .= '<html class="" lang="'. $aRouter['lang'] .'">';
-        
-        $aWidget['html'] .= '<!--- head -->';
-        $aWidget['html'] .= '<head>';
-        $aWidget['html'] .= '<meta charset="utf-8">';
-        $aWidget['html'] .= '<title>'. $aPage['title'] .'</title>';
-        
-        $aWidget['html'] .= '<link rel="stylesheet" type="text/css" href="/style/water.css">';
-        $aWidget['html'] .= '<script type="text/javascript" src="/script/home.js"></script>';
-        $aWidget['html'] .= '</head>';
-        $aWidget['html'] .= '<!--- /head -->';
-        
-        $aWidget['html'] .= '<!--- body -->';
-        $aWidget['html'] .= '<body>';
-        #$aWidget['html'] .= $aWidget['nav'];
-        
-        #$aWidget['html'] .= '<main>';
-        #$aWidget['html'] .= $aWidget['events'];
-        $aWidget['html'] .= $aPage['content'];
-        #$aWidget['html'] .= '</main>';
-        
-        $aWidget['html'] .= '</body>';
-        $aWidget['html'] .= '<!--- /body -->';
-        
-        $aWidget['html'] .= '<!--- /html -->';
-        $aWidget['html'] .= '</html>';
-        
-        print $aWidget['html'];
+global $aUri;
+$aUri = parse_url('/'. $_SERVER["REQUEST_URI"]);
+
+switch ($aUri['host']) {
+    case 'favicon.ico':
+        header('Content-Type: text/x-icon');
+        print file_get_contents(ROOT .'www'. DS .'favicon.ico');
+        exit();
+    break;
+    case 'style':
+        header('Content-Type: text/css; charset=utf-8');
+        print file_get_contents(DRAFT .'static'. DS .$aUri['path']);
+        exit();
+    break;
+    case 'script':
+        header('Content-Type: text/javascript; charset=utf-8');
+        print file_get_contents(DRAFT .'static'. DS .$aUri['path']);
+        exit();
+    break;
+    case 'index':
+        return true;
+    break;
+}
+
+$aWidget['html'] = '';
+#$aWidget['html'] .= '<!--- doctype -->';
+#$aWidget['html'] .= '<!doctype html>';
+$aWidget['html'] .= '<!--- html -->';
+$aWidget['html'] .= '<html class="" lang="'. $aRouter['lang'] .'">';
+
+$aWidget['html'] .= '<!--- head -->';
+$aWidget['html'] .= '<head>';
+$aWidget['html'] .= '<meta charset="utf-8">';
+$aWidget['html'] .= '<title>'. $aPage['title'] .'</title>';
+
+$aWidget['html'] .= '<link rel="stylesheet" type="text/css" href="/style/water.css">';
+$aWidget['html'] .= '<script type="text/javascript" src="/script/home.js"></script>';
+
+$aWidget['html'] .= '<style>
+.loader {
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-top: 4px solid #3498db;
+    border-left: 4px solid #3498db;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -20px;
+    margin-top: -20px;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
+.hidden {
+    display: none;
+}
+</style>';
+$aWidget['html'] .= '</head>';
+$aWidget['html'] .= '<!--- /head -->';
+
+$aWidget['html'] .= '<!--- body -->';
+$aWidget['html'] .= '<body>';
+#$aWidget['html'] .= $aWidget['nav'];
+
+#$aWidget['html'] .= '<main>';
+$aWidget['html'] .= '<div id="loader" class="loader"></div>';
+#$aWidget['html'] .= $aWidget['events'];
+$aWidget['html'] .= $aPage['content'];
+#$aWidget['html'] .= '</main>';
+
+$aWidget['html'] .= '</body>';
+$aWidget['html'] .= '<!--- /body -->';
+
+$aWidget['html'] .= '<!--- /html -->';
+$aWidget['html'] .= '</html>';
+
+print $aWidget['html'];
 ?>
 
 <script>
