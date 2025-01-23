f5ce9ce787af5af98bb4b2ab28a5dec880babc22
Robert-Dan <danrobert445@gmail.com>
2025-01-23 02:18:11 -0500

    default value

--- _apt.sh
+++ _apt.sh
@@ -2,6 +2,7 @@
 #php -i|grep 'php.ini'
 
 #sudo apt-get install php
-
+#sudo apt-get install sockstat
+#sockstat -l
 ps -e
 kill $(ps aux | grep '[p]hp' | awk '{print $2}')
--- www/agent.php
+++ www/agent.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace Ziel\Agent;
+namespace Ziel;
 
 class Agent {
-
-
-
+    
     public static function agent_init()
     {
         $iProcess = pcntl_fork();
@@ -14,23 +12,16 @@ class Agent {
         if ($iProcess == -1) {
             die('could not fork');
         } else if ($iProcess) {
-            // we are the parent
-            #pcntl_wait($status); //Protect against Zombie children
-            #var_dump();
-            
-            #if (!$bProcess)
-            #pcntl_exec('php -q "'.DRAFT. 'thread/worker.php"');
-            #$bProcess = true;
-            
             var_dump('we are the parent');
+            #if (!self::agent_socket()) posix_kill(getmypid(), SIGTERM);
             self::agent_socket();
-            posix_kill(getmypid(), SIGTERM);
             Agent::agent_init();
+            // we are the parent
         } else {
             var_dump('we are the child');
-            self::agent_socket();
+            #if (!self::agent_socket()) posix_kill(getmypid(), SIGTERM);
             posix_kill(getmypid(), SIGTERM);
-            Agent::agent_init();
+            #Agent::agent_init();
             // we are the child
         }
         
@@ -47,7 +38,10 @@ $port = 44321;
 // Create WebSocket.
 $server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($server, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($server, $address, $port);
+$linger = array ('l_linger' => 0, 'l_onoff' => 1);
+socket_set_option($server, SOL_SOCKET, SO_LINGER, $linger);
+
+if(!socket_bind($server, $address, $port)) {socket_close($server); self::agent_socket();}
 socket_listen($server);
 $client = socket_accept($server);
 
@@ -71,7 +65,7 @@ while (true) {
     $i++;
     if ($i == 11) #proc_close($oProcess);
     {
-        posix_kill(getmypid(), SIGTERM);
+        #posix_kill(getmypid(), SIGTERM);
         #exit();
         #exec("kill $(ps aux | grep '[p]hp' | awk '{print $2}') | sh _serve.sh &");
         #exec("kill $(ps aux | grep '[p]hp' | awk '{print $2}')");
@@ -83,7 +77,17 @@ while (true) {
     #$content = 'Now: '. $i .' '. $request.' '. time();
     $content = json_encode(array('1','2','3','4', date("H:i:s",time()) ));
     $response = chr(129) . chr(strlen($content)) . $content;
-    if(!socket_write($client, $response)) Agent::agent_init();
+    $write = socket_write($client, $response);
+    if($write === false) {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+        echo "$errorcode : $errormsg";
+        self::agent_socket();
+        return false;
+    }
+    else {
+        echo "Message Sent : $write". PHP_EOL;
+    }
     #socket_write($client, $response)
     #if the server has a client
     #if is a client restart server 
@@ -93,8 +97,8 @@ while (true) {
 return true;
 
     }
-    
 }
+
 if (php_sapi_name() == 'cli') return Agent::agent_init();
 
 
