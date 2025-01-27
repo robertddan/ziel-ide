d1cf65da2061daf8b60157af8fef7c7c851e3771
Robert-Dan <danrobert445@gmail.com>
2025-01-26 22:29:54 -0500

    default value

--- www/server.php
+++ www/server.php
@@ -86,7 +86,18 @@ $aWidget['html'] .= '<body>';
 #$aWidget['html'] .= '<main>';
 $aWidget['html'] .= <<<EOD
 <style>
-* { box-sizing: border-box; font-family: 'Calibri', sans-serif; margin: 0; padding: 0; } body { margin: 0; padding: 0; max-width: initial; } .loader { border: 16px solid #f3f3f3; /* Light grey */ border-top: 16px solid #3498db; /* Blue */ border-radius: 50%; width: 60px; height: 60px; animation: spin 2s linear infinite; position: absolute; top: 50%; left: 50%; margin-left: -20px; margin-top: -20px; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } } .hidden { display: none; } .article { display: flex; flex-flow: column nowrap; height: 100%; } .row { display: flex; height: inherit; } .side { flex: 20%; background-color: #f1f1f1; padding: 20px; } .main { flex: 80%; background-color: white; padding: 20px; textarea { height: 100%; width: 100%; resize: none; } } .footer { padding: 5px; text-align: center; background: #ddd; } /* menu */ .menu { display: flex; background-color: #303030; color: white; } .menu ul { display: flex; justify-content: space-evenly; align-items: flex-start; list-style-type: none; padding: 7px 10px; } .menu ul li { } .menu ul li a { padding: 7px 10px; text-decoration: none; text-align: center; color: #808080; } .menu ul li a:hover { color: white; } .menu ul li ul { display: none; } .menu ul li:hover ul { display: flex; position: absolute; flex-direction: column; background-color: #303030; }
+* { box-sizing: border-box; font-family: 'Calibri', sans-serif; margin: 0; padding: 0; } body { margin: 0; padding: 0; max-width: initial; } .loader { border: 16px solid #f3f3f3; /* Light grey */ border-top: 16px solid #3498db; /* Blue */ border-radius: 50%; width: 60px; height: 60px; animation: spin 2s linear infinite; position: absolute; top: 50%; left: 50%; margin-left: -20px; margin-top: -20px; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } } .hidden { display: none; } .article { display: flex; flex-flow: column nowrap; } .row { display: flex; height: inherit; } .side { flex: 20%; background-color: #f1f1f1; padding: 20px; } .main { flex: 80%; background-color: white; padding: 20px; textarea { height: 100%; width: 100%; resize: none; } } .footer { padding: 5px; text-align: center; background: #ddd; } /* menu */ .menu { display: flex; background-color: #303030; color: white; } .menu ul { display: flex; justify-content: space-evenly; align-items: flex-start; list-style-type: none; padding: 7px 10px; } .menu ul li { } .menu ul li a { padding: 7px 10px; text-decoration: none; text-align: center; color: #808080; } .menu ul li a:hover { color: white; } .menu ul li ul { display: none; } .menu ul li:hover ul { display: flex; position: absolute; flex-direction: column; background-color: #303030; }
+li.file-explorer-directory {
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    list-style-type: none;
+    margin: 7px 10px;
+}
+li.file-explorer-file {
+    list-style-type: none;
+    margin: 7px 10px;
+} 
 
 </style>
 
@@ -127,7 +138,39 @@ $aWidget['html'] .= <<<EOD
         <h3>More Text</h3>
         <div id="root"></div>
         <div id="open">open</div>
+        <div id="file-explorer" class="">
+            <ul>
+                <li class="file-explorer-file">
+                    <a href="#">File</a>
+                </li>
+                <li class="file-explorer-directory">
+                    <a href="#">Directory</a>
+                    <ul>
+                        <li class="file-explorer-file"><a id="files" href="#new">New</a></li>
+                        <li class="file-explorer-file"><a href="#save">Save</a></li>
+                        <li class="file-explorer-file"><a href="#">Save all</a></li>
+                        <li class="file-explorer-file"><a href="#">Open file</a></li>
+                        <li class="file-explorer-directory"><a href="#">Directory</a></li>
+                        <li class="file-explorer-file"><a href="#">Toggle read-only</a></li>
+                        <li class="file-explorer-file"><a href="#">Toggle read-only all</a></li>
+                    </ul>
+                </li>
+                <li class="file-explorer-file">
+                    <a href="#">Edit</a>
+                </li>
+                <li class="file-explorer-directory">
+                    <a href="#">View</a>
+                </li>
+                <li class="file-explorer-directory">
+                    <a href="#">Editor</a>
+                </li>
+                <li class="file-explorer-directory">
+                    <a href="#">Help</a>
+                </li>
+            </ul>
+        </div>
     </div>
+    
     <div class="main">
         <textarea></textarea>
     </div>
@@ -140,7 +183,7 @@ $aWidget['html'] .= <<<EOD
 </div>
 EOD;
 
-$aWidget['html'] .= '<div id="loader" class="loader"></div>';
+$aWidget['html'] .= '<div id="loader-wrapper"><div id="loader" class="loader"></div></div>';
 #$aWidget['html'] .= '<div id="sse"><a href="javascript:WebSocketSend()">send WebSocket</a></div>';
 #$aWidget['html'] .= $aWidget['events'];
 #$aWidget['html'] .= $aPage['content'];
