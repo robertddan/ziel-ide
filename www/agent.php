2a6935067716527d40101dfd2919dac6eff702d8
Robert-Dan <danrobert445@gmail.com>
2025-01-28 20:41:14 -0500

    default value

--- www/agent.php
+++ www/agent.php
@@ -2,22 +2,22 @@
 
 namespace Ziel;
 #self signed certificate
-class Agent {
-    
+class Agent
+{
     public static $sock;
-    
+
     public static function agent_init()
     {
         self::agent_native();
         #self::agent_basic();
         return true;
     }
-    
+
     public static function agent_native()
     {
-        $address = '127.0.0.1';
+        $address = "127.0.0.1";
         $port = 44321;
-        
+
         $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
         socket_bind($sock, $address, $port);
@@ -25,16 +25,15 @@ class Agent {
         $members = [];
         $connections = [];
         $connections[] = $sock;
-        
+
         echo "Listening for new connections on port $port: " . "\n";
-        
-        while(true) {
-        
+
+        while (true) {
             $reads = $writes = $exceptions = $connections;
             socket_select($reads, $writes, $exceptions, 0);
-        
-            if(in_array($sock, $reads)) {
-        /*
+
+            if (in_array($sock, $reads)) {
+                /*
         var_dump(array(
             $sock,
             $reads,
@@ -51,130 +50,139 @@ class Agent {
                     "type" => "join",
                     "sender" => "Server",
                     #"text" => "enter name to join... \n"
-                    "text" => date("Y-m-d H:i:s")
+                    "text" => date("Y-m-d H:i:s"),
                 ];
                 $reply = self::pack_data(json_encode($reply));
                 socket_write($new_connection, $reply, strlen($reply));
-                
+
                 $firstIndex = array_search($sock, $reads);
                 unset($reads[$firstIndex]);
             }
-        
+
             foreach ($reads as $key => $value) {
                 $data = socket_read($value, 1024);
-                
-                if(!empty($data)) {
+
+                if (!empty($data)) {
                     $message = self::unmask($data);
-                    #var_dump(['$message', $message]);
                     $decoded_message = json_decode($message, true);
-                    
+
+                    var_dump(['$message', $decoded_message]);
+#get cookie token for user array
+#
+
                     $decoded_message["text"] = date("Y-m-d H:i:s");
-                    $decoded_message["type"] = 'join';
-                    
+                    $decoded_message["type"] = "join";
                     $message = json_encode($decoded_message);
+                    
                     if ($decoded_message) {
-                        if(isset($decoded_message['text'])){
-                            if($decoded_message['type'] === 'join') {
+                        if (isset($decoded_message["text"])) {
+                            if ($decoded_message["type"] === "join") {
                                 $members[$key] = [
-                                    'name' => 'sender', #$decoded_message['sender'],
-                                    'connection' => $value
+                                    "name" => "sender", #$decoded_message['sender'],
+                                    "connection" => $value,
                                 ];
                             }
                             var_dump($message);
                             $maskedMessage = self::pack_data($message);
                             foreach ($members as $mkey => $mvalue) {
-                                socket_write($mvalue['connection'], $maskedMessage, strlen($maskedMessage));
+                                socket_write(
+                                    $mvalue["connection"],
+                                    $maskedMessage,
+                                    strlen($maskedMessage)
+                                );
                             }
                         }
                     }
-                }
-                else if($data === '')  {
+                } elseif ($data === "") {
                     echo "disconnected " . $key . " \n";
                     unset($connections[$key]);
-                    if(array_key_exists($key, $members)) {
+                    if (array_key_exists($key, $members)) {
                         $message = [
                             "type" => "left",
                             "sender" => "Server",
-                            "text" => $members[$key]['name'] . " left the chat \n"
+                            "text" =>
+                                $members[$key]["name"] . " left the chat \n",
                         ];
                         $maskedMessage = self::pack_data(json_encode($message));
                         unset($members[$key]);
                         foreach ($members as $mkey => $mvalue) {
-                            socket_write($mvalue['connection'], $maskedMessage, strlen($maskedMessage));
+                            socket_write(
+                                $mvalue["connection"],
+                                $maskedMessage,
+                                strlen($maskedMessage)
+                            );
                         }
                     }
                     socket_close($value);
                 }
             }
         }
-        
-        socket_close($sock);
 
-        
+        socket_close($sock);
     }
-    
-    public static function unmask($text) {
+
+    public static function unmask($text)
+    {
         $length = ord($text[1]) & 127;
-        if($length == 126) {
-    		$masks = substr($text, 4, 4);
-    		$data = substr($text, 8); 
-    	}
-        elseif($length == 127) {
-    		$masks = substr($text, 10, 4);
-    		$data = substr($text, 14); 
-    	}
-        else {
-    		$masks = substr($text, 2, 4);
-    		$data = substr($text, 6); 
-    	}
+        if ($length == 126) {
+            $masks = substr($text, 4, 4);
+            $data = substr($text, 8);
+        } elseif ($length == 127) {
+            $masks = substr($text, 10, 4);
+            $data = substr($text, 14);
+        } else {
+            $masks = substr($text, 2, 4);
+            $data = substr($text, 6);
+        }
         $text = "";
         for ($i = 0; $i < strlen($data); ++$i) {
-    		$text .= $data[$i] ^ $masks[$i % 4];    
-    	}
+            $text .= $data[$i] ^ $masks[$i % 4];
+        }
         return $text;
     }
-    
-    public static function pack_data($text) {
+
+    public static function pack_data($text)
+    {
         $b1 = 0x80 | (0x1 & 0x0f);
         $length = strlen($text);
-    
-        if($length <= 125) {
-    		$header = pack('CC', $b1, $length);
-    	}
-            
-        elseif($length > 125 && $length < 65536) {
-    		$header = pack('CCn', $b1, 126, $length);
-    	}
-            
-        elseif($length >= 65536) {
-    		$header = pack('CCNN', $b1, 127, $length);
-    	}
-            
-        return $header.$text;
-    }
-    
-    public static function handshake($request_header,$sock, $host_name, $port) {
-    	$headers = array();
-    	$lines = preg_split("/\r\n/", $request_header);
-    	foreach($lines as $line)
-    	{
-    		$line = chop($line);
-    		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches)){
-    			$headers[$matches[1]] = $matches[2];
-    		}
-    	}
-    
-    	$sec_key = $headers['Sec-WebSocket-Key'];
-    	$sec_accept = base64_encode(pack('H*', sha1($sec_key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-    	$response_header  = "HTTP/1.1 101 Switching Protocols\r\n" .
-    	"Upgrade: websocket\r\n" .
-    	"Connection: Upgrade\r\n" .
-    	"Sec-WebSocket-Accept:$sec_accept\r\n\r\n";
-    	socket_write($sock, $response_header,strlen($response_header));
+
+        if ($length <= 125) {
+            $header = pack("CC", $b1, $length);
+        } elseif ($length > 125 && $length < 65536) {
+            $header = pack("CCn", $b1, 126, $length);
+        } elseif ($length >= 65536) {
+            $header = pack("CCNN", $b1, 127, $length);
+        }
+
+        return $header . $text;
     }
 
+    public static function handshake($request_header, $sock, $host_name, $port)
+    {
+        $headers = [];
+        $lines = preg_split("/\r\n/", $request_header);
+        foreach ($lines as $line) {
+            $line = chop($line);
+            if (preg_match("/\A(\S+): (.*)\z/", $line, $matches)) {
+                $headers[$matches[1]] = $matches[2];
+            }
+        }
+
+        $sec_key = $headers["Sec-WebSocket-Key"];
+        $sec_accept = base64_encode(
+            pack("H*", sha1($sec_key . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
+        );
+        $response_header =
+            "HTTP/1.1 101 Switching Protocols\r\n" .
+            "Upgrade: websocket\r\n" .
+            "Connection: Upgrade\r\n" .
+            "Sec-WebSocket-Accept:$sec_accept\r\n\r\n";
+        socket_write($sock, $response_header, strlen($response_header));
+    }
 }
 
-if (php_sapi_name() == 'cli') return Agent::agent_init();
+if (php_sapi_name() == "cli") {
+    return Agent::agent_init();
+}
 
 ?>
--- www/server.php
+++ www/server.php
@@ -86,174 +86,10 @@ $aWidget['html'] .= '<!--- body -->';
 $aWidget['html'] .= '<body>';
 $aWidget['html'] .= <<<EOD
 <style>
-body {
-	font-family: 'Calibri', sans-serif;
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
-	margin: 0;
-	padding: 0;
-}
-header {
-	flex: 0 0 0;
-	background-color: #C14F4F;
-}
-main {
-	flex: 1;
-	display: flex;
-	background-color: #699EBD;
-    height: 86%;
-}
-footer {
-	flex: 0 0 40px;
-	background-color: #C14F4F;
-	text-align: center;
-}
-.left,
-.right {
-	flex: 0 2 25%;
-	background-color: #C28282;
-    height: 100%;
-    overflow: hidden;
-}
-.middle {
-	flex: 1 1 75%;
-	padding-left: 50px;
-}
-.loader {
-	border: 16px solid #f3f3f3;
-	border-top: 16px solid #3498db;
-	border-radius: 50%;
-	width: 60px;
-	height: 60px;
-	animation: spin 2s linear infinite;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	margin-left: -20px;
-	margin-top: -20px;
-}
-@keyframes spin {
-	0% {
-		transform: rotate(0deg);
-	}
-
-	100% {
-		transform: rotate(360deg);
-	}
-
-}
-.hidden {
-	display: none;
-}
-
-.menu {
-	display: flex;
-	background-color: #303030;
-	color: white;
-}
-.menu ul {
-	display: flex;
-	justify-content: space-evenly;
-	align-items: flex-start;
-	list-style-type: none;
-	padding: 7px;
-	margin: 5px;
-}
-.menu ul li {
-}
-.menu ul li a {
-	padding: 7px 14px;
-	text-decoration: none;
-	text-align: center;
-	color: #808080;
-}
-.menu ul li a:hover {
-	color: white;
-}
-.menu ul li ul {
-	display: none;
-}
-.menu ul li:hover ul {
-	display: flex;
-	position: absolute;
-	flex-direction: column;
-	background-color: #303030;
-	padding-top: 7px;
-}
-
-.menu ul li:hover ul li{
-	padding: 7px 14px;
-}
-
-@font-face {
-  font-family: "Fira Code";
-  src: url("/fonts/FiraCode-Regular.ttf");
-}
-textarea {
-    font-family: "Fira Code";
-    font-size: 16px;
-    height: 100%;
-	width: 100%;
-	resize: none;
-	border: none;
-}
-textarea:focus {
-    outline: none;
-    border: none;
-}
-#file-explorer > div {
-    padding-bottom: 220px;
-}
-#file-explorer {
-    overflow: scroll;
-    height: 100%;
-    scrollbar-color: red orange;
-    scrollbar-width: thin;
-    background-color: lightyellow;
-}
-#file-explorer div {
-    display: flex;
-    flex-direction: column;
-}
-#file-explorer li {
-	list-style-type: none;
-}
-#file-explorer a {
-	text-decoration: none;
-	color: #303030;
-}
-#file-explorer a:hover {
-
-}
-#file-explorer span {
-    background-color: lightyellow;
-	cursor: pointer;
-    border-top: 1px solid gray;
-}
-#file-explorer span:hover {
-    background-color: aliceblue;
-}
-.file-explorer-directory {
-    padding: 7px;
-}
-.file-explorer-directory:hover {
-
-}
-.file-explorer-file {
-    padding: 7px;
-}
-.file-explorer-directory + div {
-    margin-left: 14px;
-    border-left: 1px solid gray;
-}
-
-.file-explorer-directory:hover {
-
-}
-.file-explorer div span:nth-last-child(){
-    border-bottom: 1px solid gray;
-}
+body { font-family: 'Calibri', sans-serif; display: flex; flex-direction: column; min-height: 100vh; margin: 0; padding: 0; } header { flex: 0 0 0; background-color: #C14F4F; } main { flex: 1; display: flex; background-color: #699EBD; height: 86%; } footer { flex: 0 0 40px; background-color: #C14F4F; text-align: center; } .left, .right { flex: 0 2 25%; background-color: #C28282; height: 100%; overflow: hidden; } .middle { flex: 1 1 75%; padding-left: 50px; } .loader { border: 16px solid #f3f3f3; border-top: 16px solid #3498db; border-radius: 50%; width: 60px; height: 60px; animation: spin 2s linear infinite; position: absolute; top: 50%; left: 50%; margin-left: -20px; margin-top: -20px; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } } .hidden { display: none; } @font-face { font-family: "Fira Code"; src: url("/fonts/FiraCode-Regular.ttf"); } textarea { font-family: "Fira Code"; font-size: 16px; height: 100%; width: 100%; resize: none; border: none; } textarea:focus { outline: none; border: none; }
+.menu { display: flex; background-color: #303030; color: white; } .menu ul { display: flex; justify-content: space-evenly; align-items: flex-start; list-style-type: none; padding: 7px; margin: 5px; } .menu ul li { } .menu ul li a { padding: 7px 14px; text-decoration: none; text-align: center; color: #808080; } .menu ul li a:hover { color: white; } .menu ul li ul { display: none; } .menu ul li:hover ul { display: flex; position: absolute; flex-direction: column; background-color: #303030; padding-top: 7px; } .menu ul li:hover ul li{ padding: 7px 14px; }
+#file-explorer > div { padding-bottom: 220px; } #file-explorer { overflow: scroll; height: 100%; scrollbar-color: red orange; scrollbar-width: thin; background-color: lightyellow; } #file-explorer div { display: flex; flex-direction: column; } #file-explorer li { list-style-type: none; } #file-explorer a { text-decoration: none; color: #303030; } #file-explorer a:hover { } #file-explorer span { background-color: lightyellow; cursor: pointer; border-top: 1px solid gray; } #file-explorer span:hover { background-color: aliceblue; } .file-explorer-directory { padding: 7px; } .file-explorer-directory:hover { } .file-explorer-file { padding: 7px; } .file-explorer-directory + div { margin-left: 14px; border-left: 1px solid gray; } .file-explorer-directory:hover { } .file-explorer div span:nth-last-child(){ border-bottom: 1px solid gray; }
+nav { height: 40px; background-color: darkslategrey; }
 
 </style>
 
@@ -263,13 +99,13 @@ textarea:focus {
         <li>
             <a href="#">Files</a>
             <ul>
-                <li><a id="ide-files-new" href="#new">New</a></li>
-                <li><a href="#save">Save</a></li>
-                <li><a href="#">Save all</a></li>
-                <li><a href="#">Open file</a></li>
-                <li><a href="#">Open project</a></li>
-                <li><a href="#">Toggle read-only</a></li>
-                <li><a href="#">Toggle read-only all</a></li>
+                <li><a data-menu-files="new" href="#new">New</a></li>
+                <li><a data-menu-files="save" href="#save">Save</a></li>
+                <li><a data-menu-files="saveAll" href="#">Save all</a></li>
+                <li><a data-menu-files="openFile" href="#">Open file</a></li>
+                <li><a data-menu-files="openProject" href="#">Open project</a></li>
+                <li><a data-menu-files="readOnly" href="#">Toggle read-only</a></li>
+                <li><a data-menu-files="readOnlyAll" href="#">Toggle read-only all</a></li>
             </ul>
         </li>
         <li>
@@ -287,6 +123,7 @@ textarea:focus {
     </ul>
 </div>
 </header>
+<nav></nav>
 <main>
     <div class="left">
         <h3>Draft directory name</h3>
@@ -397,23 +234,34 @@ socket.addEventListener("message", socketMessage);
 
 
 let menu = {
-    oninit: function() {
+    onInit: function() {
+        let links = document.querySelectorAll('.menu a[data-menu-files]');
+        links.forEach((link) => {
+            var action = link.getAttribute('data-menu-files');
+            //var action = link.getAttribute('data-menu-files');
+            //console.log(link.getAttribute('data-menu-files'));
+            link.addEventListener('click', (event) => {
+                var message = '{"menu":"'+ action +'", "file":""}';
+                socket.call(message);
+            });
+        });
     },
-    files.new: : function() {
+    filesNew: function() {
     },
-    files.save: : function() {
+    filesSave: function() {
     },
-    files.saveAll: : function() {
+    filesSaveAll: function() {
     },
-    files.open: : function() {
+    filesOpen: function() {
     },
-    files.openProject: : function() {
+    filesOpenProject: function() {
     },
-    files.readOnly: : function() {
+    filesReadOnly: function() {
     },
-    files.readOnlyAll: : function() {
+    filesReadOnlyAll: function() {
     }
 }
+
 
 let nav = {
     oninit: function() {
@@ -427,6 +275,15 @@ let main = {
 
 let aside = {
     oninit: function() {
+        links.forEach((link) => {
+            var action = link.getAttribute('data-menu-files');
+            //var action = link.getAttribute('data-menu-files');
+            //console.log(link.getAttribute('data-menu-files'));
+            link.addEventListener('click', (event) => {
+                var message = '{"menu":"'+ action +'", "file":""}';
+                view.call(message);
+            });
+        });
     }
 }
 
@@ -434,72 +291,75 @@ let footer = {
     oninit: function() {}
 }
 
-
-let sockets = {
-    socket: {},
+let socket = {
+    peer: {},
 	host: 'ws://127.0.0.1:44321/www/agent.php',
-	oninit: function() {
-        this.socket = new WebSocket(this.host);
-        this.socket.addEventListener('readystatechange', this.state);
-        this.socket.addEventListener('open', this.open);
-        this.socket.addEventListener('message', this.message);
-        this.socket.addEventListener('close', this.close);
-        this.socket.addEventListener('error', this.error);
+	onInit: function() {
+        this.peer = new WebSocket(this.host);
+        this.peer.addEventListener('readystatechange', this.onState);
+        this.peer.addEventListener('open', this.onOpen);
+        this.peer.addEventListener('message', this.onMessage);
+        this.peer.addEventListener('close', this.onClose);
+        this.peer.addEventListener('error', this.onError);
     },
-    close: function(event) {
+    call: function(event) {
+        console.log("WebSocket send: ", event);
+        socket.peer.send(event);
+    },
+    onClose: function(event) {
         console.log("WebSocket close: ", event);
         document.getElementById('loader').classList.remove("hidden");
         //setInterval(1000);
-        //sockets.oninit();
+        //socket.onInit();
     },
-    error: function(error) {
+    onError: function(error) {
         console.log("WebSocket error: ", event);
         document.getElementById('loader').classList.remove("hidden");
         //setInterval(1000);
-        //sockets.oninit();
+        //socket.onInit();
     },
-    message: function(event) {
+    onMessage: function(event) {
         console.log('WebSocket onmessage: ', event);
         document.getElementById('root').innerHTML = "Message from server "+ event.data;
     },
-    open: function(event) {
+    onOpen: function(event) {
         console.log("WebSocket open: ", event);
-        var message = '{"a":1101,"b":22}';
         document.getElementById('loader').classList.add("hidden");
+/*
+        var message = '{"a":1101,"b":22}';
         document.getElementById('open').innerHTML = JSON.stringify(event.data);
         
         setInterval(function() {
             if (event.currentTarget['bufferedAmount'] == 0)
-            sockets.socket.send(message);
+            socket.peer.send(message);
             console.log(message);
         }, 10000);
-/*        
-var data = new ArrayBuffer(10000000);
-sockets.socket.send(data);
-if (event.currentTarget['bufferedAmount'] === 0) {
-console.log('the data sent');
-}
-else {
-console.log('the data did not send');
-}
+        //var data = new ArrayBuffer(10000000);
 */
     },
-    state: function(state) {
+    onState: function(state) {
         // this.socket.readyState
         console.log("WebSocket state: ", state);
     }
 }
 
+let layer = {
+    onInit: function(state) {
+        window.addEventListener("beforeunload", this.beforeunload);
+        document.addEventListener("DOMContentLoaded", this.contentloaded);
+    },
+    beforeunload: function(event) {
+        document.getElementById('loader').classList.remove("hidden");
+    },
+    contentloaded: function(event) {
+        socket.onInit();
+        document.getElementById('loader').classList.remove("hidden");
+    }
+}
 
-window.addEventListener("beforeunload", (event) => {
-    document.getElementById('loader').classList.remove("hidden");
-});
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById('loader').classList.remove("hidden");
-    sockets.oninit();
-    //console.log(sockets.socket.readyState);
-});
+menu.onInit()
+layer.onInit();
 
 </script>
 
