2a156c794a20ca24dde987b7669ac529dc48095c
Robert-Dan <danrobert445@gmail.com>
2025-01-29 22:52:41 -0500

    default value

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -5,9 +5,16 @@ class Autoload {
     public static $sJsonName = "ziel.json";
     public static $aDirectories = array();
     public static $aClasses = array();
+    public static $aComposer = array();
+    
+    
+    public static $i = 0;
     
     public static function autoload_custom()
     {
+        
+        #return var_dump(self::$aClasses);
+        
         #spl_autoload_register(__CLASS__ ."::". __FUNCTION__);
         foreach(self::$aClasses as $sClass) 
         if(!require($sClass)) throw_exception('autoload_custom()');
@@ -19,7 +26,7 @@ class Autoload {
     public static function autoload_json()
     {
         $sComposer = file_get_contents(ROOT . self::$sJsonName);
-        $aComposer = json_decode($sComposer, true);
+        self::$aComposer = json_decode($sComposer, true);
         
         #for json require
         #$aComposer['autoload']['psr-4']
@@ -31,46 +38,68 @@ class Autoload {
         ]);
         */
         
-        foreach($aComposer['autoload']['psr-4'] as $sNamespace => $aDirectory)
+        foreach(self::$aComposer['autoload']['psr-4'] as $sNamespace => $aDirectory)
         {
             if (!is_array($aDirectory)) 
             $aDirectory = (array) $aDirectory;
             foreach($aDirectory as $sDirectory) 
             {
                 if (!is_dir(ROOT . trim($sDirectory))) continue;
-                self::$aDirectories[] = array($sNamespace, ROOT. trim($sDirectory) .DS);
+                self::$aDirectories[] = array('namespace' => $sNamespace, 
+                    'directory' => $sDirectory, 
+                    'path' => ROOT. trim($sDirectory) .DS);
             }
         }
-        
-        
         return true;
     }
     
-    public static function autoload_namespaces($aVendors = array())
+    public static function autoload_namespaces($aaVendors = array())
     {
         #$aDirectories = array_push($aDirectories, __DIR__);
         #self::$aDirectories
 echo '<pre>';
+        #var_dump(self::$aDirectories);
         
-        if (empty($aVendors)) $aaVendors = self::$aDirectories;
-        if (empty($aaVendors)) return true;
-            
+        if (empty($aaVendors)) 
+        {
+            $aaVendors = self::$aDirectories;
+            if (empty($aaVendors)) return true;
+        } 
+        
+        #var_dump(['foreach($aaVendors as $aVendor)', $aaVendors]);
+        
         foreach($aaVendors as $aVendor)
         {
-            var_dump([$aaVendors ,$aVendor]);
+            #var_dump([scandir($aVendor[1]), $aVendor, $aaVendors]);
+            #var_dump($aVendor);
+            $aPaths = array_diff(scandir($aVendor['path']), array('.', '..', 'autoload.php'));
             
-            $aClasses = array_diff(scandir($aVendor[1]), array('.', '..', 'autoload.php'));
+            #var_dump([$aPaths ,$aVendor]);
             
-
-            foreach ($aClasses as $sClass) {
-                var_dump($aVendor[1] . $sClass);
-                if(is_dir($aVendor[1] . $sClass)) {
+            foreach ($aPaths as $sPath) {
+                
+                if(is_dir($aVendor[1] . $sPath)) {
+                    #var_dump([
+                        #$aVendor[1].$sPath, 
+                        #is_dir($aVendor[1] . $sPath),
+                        #array($aVendor[0], $aVendor[1] .$sPath)
+                    #]);
+                    self::$i++;
+                    $aaVendor = array(array($aVendor['namespace'], $aVendor['path'] .$sPath. DS));
+                    self::autoload_namespaces($aaVendor);
+                }
+                else {
+                    if(!in_array(pathinfo($aVendor['path'] .$sPath)['extension'], array('php'))) continue;
+                    #expllode($aVendor[1]);
+                    #var_dump(self::$aComposer);
+                    $sVendorPath = $aVendor['path'] .$sPath. DS;
+                    $sNameSpace = explode('draft',$sVendorPath);
                     
-                    self::autoload_namespaces(array($aVendor[0], $aVendor[1] .$sClass));
-var_dump([
-    is_dir($aVendor[1] . $sClass),
-    
-]);
+                    #var_dump(self::$aComposer);
+                    
+                    $aClass = array($aVendor['namespace'], ucfirst(array_shift(explode('.',$sPath))), $aVendor['path'] .$sPath. DS);
+                    if(!array_push(self::$aClasses, $aClass)) continue;
+                    #var_dump($aVendor[1] .$sPath);
                 }
             }
             
