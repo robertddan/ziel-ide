06ba1dc666396cd7383ede5337aeaa0b38b3a184
Robert-Dan <danrobert445@gmail.com>
2025-02-08 21:48:59 -0500

    default value

--- public/server.php
+++ public/server.php
@@ -1,5 +1,8 @@
 <?php
+/*
+Docker containers
 
+*/
 function exception_handler (Throwable $exception) {
     print PHP_EOL. $exception->getMessage() .PHP_EOL.
     "On file: ".$exception->getFile() .PHP_EOL.
@@ -96,7 +99,7 @@ $aWidget['html'] .= '<meta charset="utf-8">';
 #$aWidget['html'] .= '<title>'. $aPage['title'] .'</title>';
 #$aWidget['html'] .= '<script type="text/javascript" src="/script/home.js"></script>';
 $aWidget['html'] .= '<style>.loader { border: 16px solid #f3f3f3; /* Light grey */ border-top: 16px solid #3498db; /* Blue */ border-radius: 50%; width: 60px; height: 60px; animation: spin 2s linear infinite; position: absolute; top: 50%; left: 50%; margin-left: -20px; margin-top: -20px; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } } .hidden { display: none; }</style>';
-$aWidget['html'] .= '<link rel="manifest" href="/app.webmanifest">';
+#$aWidget['html'] .= '<link rel="manifest" href="/app.webmanifest">';
 $aWidget['html'] .= '</head>';
 $aWidget['html'] .= '<!--- /head -->';
 $aWidget['html'] .= '<!--- body -->';
